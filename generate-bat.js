19b69c5365f15350ddae0b036ac4774093ea6914
kucingscript <kucingscript@gmail.com>
2026-04-23 11:50:57 +0700

    generate bat

--- generate-bat.js
+++ generate-bat.js
@@ -2,16 +2,10 @@ const fs = require("fs");
 const path = require("path");
 const os = require("os");
 
-// 1. Deteksi direktori project (lokasi script ini)
 const projectDir = __dirname;
-
-// 2. Ambil path Desktop user
 const desktopPath = path.join(os.homedir(), "Desktop");
-
-// 3. Nama file .bat
 const batFilePath = path.join(desktopPath, "auto-layout-stock.bat");
 
-// 4. Isi file .bat (gunakan path dinamis)
 const batContent = `@echo off
 cd /d "${projectDir}"
 
@@ -24,7 +18,6 @@ npm start
 pause
 `;
 
-// 5. Tulis file
 fs.writeFileSync(batFilePath, batContent, "utf8");
 
 console.log("✅ File .bat berhasil dibuat di:");
